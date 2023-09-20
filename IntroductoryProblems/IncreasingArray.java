d6727546137e7c47370f26a29247c15f1ca5e009
Dheeruyendra Pratap Singh <56476885+Dheeruyendra@users.noreply.github.com>
2023-09-20 23:06:05 +0530

    increasing array

---
+++ IntroductoryProblems/IncreasingArray.java
@@ -0,0 +1,30 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class IncreasingArray {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+
+    String[] str = br.readLine().split("\\s+");
+
+    long[] arr = new long[n];
+    for(int i=0; i<n; i++){
+        arr[i] = Long.parseLong(str[i]);
+    }
+
+    long moves = 0;
+    for(int i=1; i<n; i++){
+        if(arr[i] < arr[i-1]){
+         moves+=(arr[i-1]-arr[i]);
+         arr[i] += (arr[i-1]-arr[i]);
+        } 
+    }
+   
+     System.out.println(moves);
+  }
+    
+}
